7e5beacbdfc80776e14eb084569dc9f807ba798f
John Robinson <john.robinson@wellzesta.com>
2014-10-07 15:59:28 -0500

    added CC-to for email
    Also fixed the echo

--- www/paleo/send_email.php
+++ www/paleo/send_email.php
@@ -11,14 +11,16 @@ $email_message = "Contents:\n\n\n";
 $email_message .= "Name: ".$email_name."\n";
 $email_message .= "Email: ".$email_from."\n";
 $email_message .= $json."\n";   
+
+$email_to .= $email_from; // CC to sender
+
 // create email headers
 $headers = 'From: '.$email_from."\n".
 'Reply-To: '.$email_from."\n" .
 'X-Mailer: PHP/' . phpversion();
-echo "$email_message";
 @mail($email_to, $email_subject, $email_message, $headers);
 $response = 'Email sent to: '.$email_to.' and CC to '.$email_name.' <'.$email_from.'>';
-echo $response ;
+echo "$response";
 }
 else {
 echo "Email send failure." ;
