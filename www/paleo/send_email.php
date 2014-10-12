7901546191f09115e28b38632b3e9604882be363
John Robinson <john.robinson@wellzesta.com>
2014-10-12 16:55:22 -0500

    fixed send_email.php

--- www/paleo/send_email.php
+++ www/paleo/send_email.php
@@ -7,8 +7,8 @@ $email_subject = "New Paleo Recipe";
 $email_name = $_POST['name']; // required
 $email_from = $_POST['email']; // required
 $json = $_POST['jstr'];
-$email_message =  '// '.$email_subject.' submitted by '.$email_name.' <'.$email_from.'>\n';
-$email_message .= $json.'\n';   
+$email_message =  "// ".$email_subject." submitted by ".$email_name." <".$email_from.">\n";
+$email_message .= $json."\n";   
 
 $email_to = $email_me.",".$email_from; // CC to sender
 
@@ -17,7 +17,7 @@ $headers = 'From: '.$email_from."\n".
 'Reply-To: '.$email_from."\n" .
 'X-Mailer: PHP/' . phpversion();
 @mail($email_to, $email_subject, $email_message, $headers);
-$response = 'Email sent to: '.$email_me.' and CC to '.$email_name.' <'.$email_from.'>';
+$response = "Email sent to: ".$email_me." and CC to ".$email_name." <".$email_from.">";
 echo "$response";
 }
 else {
